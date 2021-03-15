392e95a15f25a76aaa2cb773888dcdf841bed067
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-03-15 14:10:35 -0700

    workin on counter js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,25 +46,33 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
-export default function Counter() {
-  /* STEP 1 */
+export default function Counter(props) {
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState(false)
 
   const increment = () => {
-    /* STEP 4 */
+    setCount(count + 1)
   };
+
   const decrement = () => {
-    /* STEP 5 */
+    setCount(count - 1)
   };
+
+
   const reset = () => {
-    /* STEP 6 */
+
   };
 
   const style = {
-    fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: 'royalblue',
+    if(color) {
+      return (
+        even
+      )
+    }
   };
 
   return (
